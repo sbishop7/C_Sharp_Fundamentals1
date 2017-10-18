c03c890368429343a50e4e5ea86f228e9331e288
sbishop7 <seth.bishop@gmail.com>
2017-10-18 16:05:02 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fundamentals1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
